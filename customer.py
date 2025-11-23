ac8298846348b0ed4434457d6c78a4726bd8f4c2
Bonarerijt <prudyjj4@gmail.com>
2025-11-23 16:06:23 +0300

    Initialized a customer with the property name and the necessary validation

--- customer.py
+++ customer.py
@@ -1,2 +1,10 @@
 class Customer:
-    pass
+    def __init__(self, name):
+        if not isinstance(name, str) or not (1 <= len(name) <= 15):
+            raise ValueError(
+                "Name must be a string between 1 and 15 characters")
+        self.name = name
+
+
+person1 = Customer(6)
+print(person1.name)
